dcd8c9c6b2f3b5d3369067c6f736666c0efddf68
AmrMSaid <amrmohamed4422@gmail.com>
2026-07-04 20:35:12 +0300

    feat(auth): add AuthFooter component

---
+++ src/features/auth/components/auth-footer.tsx
@@ -0,0 +1,33 @@
+import { Link } from '@/i18n/navigation';
+import { Separator } from '@/shared/ui/separator';
+
+type Props = {
+  text: string;
+  linkText?: string;
+  href?: string | null;
+};
+
+export default function AuthFooter({ text, linkText, href }: Props) {
+  return (
+    <div className="mt-9 flex w-full flex-col content-center items-center justify-center">
+      <Separator />
+
+      <div className="mt-5 flex gap-1 text-sm font-medium">
+        <span>{text}</span>
+
+        {href && linkText ? (
+          <Link
+            href={href}
+            className="text-ds-primary hover:text-ds-primary-saturated text-sm font-bold"
+          >
+            {linkText}
+          </Link>
+        ) : (
+          linkText && (
+            <span className="text-ds-primary cursor-not-allowed text-sm font-bold">{linkText}</span>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
