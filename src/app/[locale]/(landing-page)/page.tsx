dba3a78ae05773c2e2b9e421c1651539f66acdfd
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2026-07-14 20:09:35 +0300

    feat(landing): implement fully localised Hero and Features sections

--- src/app/[locale]/(landing-page)/page.tsx
+++ src/app/[locale]/(landing-page)/page.tsx
@@ -1,13 +1,11 @@
-import ProductCard from '@/features/landing-page/components/product-card';
-import { PRODUCT_CARD_DUMMY_DATA } from '@/features/landing-page/lib/constants/product.constant';
-import ProductCardSkeleton from '@/features/landing-page/skeletons/product-card.skeleton';
+import Features from '@/features/landing-page/components/features';
+import Hero from '@/features/landing-page/components/hero/hero';
 
 export default async function LandingPage() {
   return (
-    <div className="space-y-4 p-2">
-      <ProductCard product={PRODUCT_CARD_DUMMY_DATA} />
-
-      <ProductCardSkeleton />
-    </div>
+    <>
+      <Hero />
+      <Features />
+    </>
   );
 }
