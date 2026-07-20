18df03d890c42dbd411141ed3e96ab6bcfdafa6b
MahmoudAbdulla7 <mahmoudapdallah629@gmail.com>
2026-07-20 14:00:37 +0300

    feat(products): scaffold products page layout with filters and content sections

--- src/app/[locale]/(landing-page)/products/page.tsx
+++ src/app/[locale]/(landing-page)/products/page.tsx
@@ -1,3 +1,15 @@
+import ProductsContent from '@/features/products/components/products-content/products-content';
+import Filters from '@/features/products/components/sidebar/filters';
+
 export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return (
+    <main className="flex-raw container flex gap-6.25 overflow-hidden py-6">
+      <section className="w-1/4 rounded-lg">
+        <Filters />
+      </section>
+      <section className="w-3/4 rounded-lg">
+        <ProductsContent />
+      </section>
+    </main>
+  );
 }
