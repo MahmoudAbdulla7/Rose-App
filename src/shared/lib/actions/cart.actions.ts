36ece43defc9f92c5ba2db99ae0a866838ed1f9e
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2026-07-24 02:30:34 +0300

    fix: update types for custom hooks

--- src/shared/lib/actions/cart.actions.ts
+++ src/shared/lib/actions/cart.actions.ts
@@ -3,7 +3,7 @@
 import { buildApiEndpoint } from '../utils/api-endpoint-builder.utils';
 import { getNextAuthToken } from '../utils/auth.utils';
 import { API_HEADERS } from '../apis/headers.options';
-import type { ICartItem, IAddToCart, IRemoveFromCart } from '../types/cart';
+import type { ICartItem, IAddToCart, IRemoveFromCart, IUpdateCartQuantity } from '../types/cart';
 
 export async function addToCart(data: IAddToCart): Promise<IAPIResponse<ICartItem>> {
   const token = await getNextAuthToken();
@@ -39,6 +39,29 @@ export async function removeFromCart(data: IRemoveFromCart): Promise<IAPIRespons
 
   const response = await fetch(endpoint.toString(), {
     method: 'DELETE',
+    headers: {
+      ...API_HEADERS.JSON,
+      ...API_HEADERS.AUTHORIZATION(token.accessToken),
+    },
+  });
+
+  return (await response.json()) as IAPIResponse<ICartItem>;
+}
+
+export async function updateCartQuantity(
+  data: IUpdateCartQuantity,
+): Promise<IAPIResponse<ICartItem>> {
+  const token = await getNextAuthToken();
+
+  if (!token) {
+    throw new Error('Unauthorized');
+  }
+
+  const endpoint = buildApiEndpoint('/cart', {});
+
+  const response = await fetch(endpoint.toString(), {
+    method: 'PATCH', // or 'PUT' depending on your API
+    body: JSON.stringify(data),
     headers: {
       ...API_HEADERS.JSON,
       ...API_HEADERS.AUTHORIZATION(token.accessToken),
