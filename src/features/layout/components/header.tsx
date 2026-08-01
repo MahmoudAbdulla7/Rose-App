158fd1859082c98899225da4d4d9b861cc8eaf43
MahmoudAbdulla7 <mahmoudapdallah629@gmail.com>
2026-08-01 19:09:17 +0300

    fix(build): merge wishlist correctly

--- src/features/layout/components/header.tsx
+++ src/features/layout/components/header.tsx
@@ -7,10 +7,11 @@ import { Link } from '@/i18n/navigation';
 import LanguageSwitcherComponent from '@/shared/components/language-switcher';
 import ThemeToggle from '@/shared/components/theme-toggle';
 import { Separator } from '@/shared/ui/separator';
-import { Bell, Heart } from 'lucide-react';
+import { Bell } from 'lucide-react';
 import { getLocale, getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import HeaderCartLink from './header-cart-link';
+import HeaderWishlistLink from './header-wishlist-link';
 
 export default async function Header() {
   const t = await getTranslations('header');
