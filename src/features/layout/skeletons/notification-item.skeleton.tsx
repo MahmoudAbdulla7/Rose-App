66cd72a5d0aa49e5ea2ffbc4c9770590f398e9ff
AmrMSaid <amrmohamed4422@gmail.com>
2026-08-06 11:35:52 +0300

    refactor(notifications): reorganize components and improve code structure

--- src/features/layout/skeletons/notification-item.skeleton.tsx
+++ src/features/layout/skeletons/notification-item.skeleton.tsx
@@ -1,13 +1,23 @@
 import Skeleton from '@/shared/ui/skeleton';
 
-export default function NotificationItemSkeleton() {
-  return (
-    <div className="flex min-h-30.5 flex-col gap-1.5 p-4">
-      {/* Title */}
-      <Skeleton className="bg-ds-soft h-5 w-3/4" />
+interface NotificationItemSkeletonProps {
+  count?: number;
+}
 
-      {/* Message */}
-      <Skeleton className="bg-ds-soft h-14 w-full" />
-    </div>
+export default function NotificationItemSkeleton({ count = 1 }: NotificationItemSkeletonProps) {
+  const items = Array.from({ length: count });
+
+  return (
+    <>
+      {items.map((_, index) => (
+        <div key={index} className="flex min-h-30.5 flex-col gap-1.5 p-4">
+          {/* Title */}
+          <Skeleton className="bg-ds-soft h-5 w-3/4" />
+
+          {/* Message */}
+          <Skeleton className="bg-ds-soft h-14 w-full" />
+        </div>
+      ))}
+    </>
   );
 }
