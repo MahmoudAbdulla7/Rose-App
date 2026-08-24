adcff65198db1be87ebe4ef96491d0301d688608
Ahmednaga2001 <ahmednaga02001@gmail.com>
2026-08-24 09:33:23 +0300

    feat(admin, landing-page): add new pages and components for dashboard and user landing features

--- src/app/[locale]/@admin/categories/page.tsx
+++ src/app/[locale]/@admin/categories/page.tsx
@@ -1,8 +1,6 @@
 import { useTranslations } from 'next-intl';
 
-import type { ReactNode } from 'react';
-
-export default function DashboardCategoriesPage(): ReactNode {
+export default function DashboardCategoriesPage() {
   const t = useTranslations('dashboard.nav');
 
   return <h1 className="text-ds-text-plain text-2xl font-semibold">{t('categories')}</h1>;
