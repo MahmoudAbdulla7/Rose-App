887b4a7666894ad490170ebe368e712d7857e06a
AmrMSaid <amrmohamed4422@gmail.com>
2026-08-25 22:17:00 +0300

    refactor(dashboard): use suspense for overview loading

--- src/app/[locale]/@admin/loading.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardOverviewSkeleton from '@/features/dashboard/skeletons/dashboard-overview.skeleton';
-
-export default function Loading() {
-  return <DashboardOverviewSkeleton />;
-}
